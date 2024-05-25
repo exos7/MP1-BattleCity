5d52f868811a439370c7f3c6e063e9cef502eeaa
Gabriel Ian Carl Matias <gamatias@up.edu.ph>
2024-05-25 16:49:51 +0800

    add enemy tank :D

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pyxel
 from tank import Tank
+from enemy import Enemy
 
 class App:
     def __init__(self):
@@ -7,13 +8,17 @@ class App:
         self.x = 0
         self.y = 0
         self.tank = Tank(self.x, self.y)
+        self.enemy = Enemy(20,20)
         pyxel.load('PYXEL_RESOURCE_FILE.pyxres')
         pyxel.run(self.update, self.draw)
 
     def update(self):
         if self.tank.isAlive == True:
             self.tank.update()
+            self.enemy.update()
     def draw(self):
         self.tank.draw()
+        self.enemy.draw()
+        # x.draw()
 
 App()
---
+++ enemy.py
@@ -0,0 +1,44 @@
+from functions import atBottom, atTop, atLeft, atRight
+import random
+import pyxel
+
+MoveSpeed = 4
+
+class Enemy:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.u, self.v = 0, 0
+        self.isAlive = True
+
+    def in_bounds(self, x, y):
+        if 0 <= x <= pyxel.width and 0 <= y <= pyxel.height:
+            return True
+
+    def update(self):
+        DirInt = random.randint(0, 3)   
+        if self.in_bounds(self.x, self.y):
+            if DirInt == 0 and not atTop(self.x, self.y):
+                print('w')
+                self.u = 0
+                self.v = 32
+                self.y -= MoveSpeed
+            if DirInt == 1 and not atLeft(self.x, self.y):
+                print('a')
+                self.u = 16
+                self.v = 48
+                self.x -= MoveSpeed
+            if DirInt == 2 and not atBottom(self.x, self.y):
+                print('s')
+                self.u = 0
+                self.v = 48
+                self.y += MoveSpeed
+            if DirInt == 3 and not atRight(self.x, self.y):
+                print('d')
+                self.u = 16
+                self.v = 32
+                self.x += MoveSpeed
+                  
+    def draw(self):
+        # pyxel.cls(0)
+        pyxel.blt(self.x, self.y, 1, self.u, self.v, 16, 16)
